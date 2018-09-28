40db390c50af9e898268ef540da3c6404f446296
Líkið Geimfari <likid.geimfari@gmail.com>
2018-09-28 17:00:34 +0300

    Update sosatb.js

--- sosatb.js
+++ sosatb.js
@@ -1,6 +1,5 @@
 (function roskomnadzorSosatb() {
     ['container', 'wrapper'].forEach(function(id) {
-        console.log('Роскомнадзор сасатб!')
         document.getElementById('age-verification-' + id).outerHTML = "";
     });
 })()
